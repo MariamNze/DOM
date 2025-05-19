4775415a8f1f47e9d4a7497966b6d9640601b403
Mariam Nzeyimana <Mariam.Nze@gmail.com>
2025-05-19 19:14:29 +0200

    Added removeClassFromElement to exercice

--- addClassToElement/index.js
+++ addClassToElement/index.js
@@ -6,3 +6,10 @@ newBox.classList.add('box');
 const container = document.querySelector('.container-boxes');
 
 container.appendChild(newBox);
+
+
+/* To remove the new box that was created:
+
+newBox.classList.remove('box');
+
+*/
--- addRemoveClass/index.js
+++
@@ -1,8 +0,0 @@
-const newBox = document.createElement('div');
-
-newBox.classList.add('box');
-
-
-const container = document.querySelector('.container-boxes');
-
-container.appendChild(newBox);
