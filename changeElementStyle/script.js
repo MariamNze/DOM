f5714c0e81e270c39fe34a250ecfffde349dd68d
Mariam Nzeyimana <Mariam.Nze@gmail.com>
2025-05-19 18:37:31 +0200

    Ended changeElementStyle exercice

---
+++ changeElementStyle/script.js
@@ -0,0 +1,7 @@
+const box = document.querySelector('.box');
+
+box.style.backgroundColor = "yellow";
+box.style.height = "300px";
+box.style.width = "400px";
+box.style.fontSize = "60px";
+box.style.color = "lightblue";
